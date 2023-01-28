e20eff1b8aaf483799eb8cb573172f8296c3bf92
cheekw <kweic8@gmail.com>
2023-01-28 13:28:50 -0800

    generic test function modification

--- opencv/node-test/index.js
+++ opencv/node-test/index.js
@@ -1,36 +1,42 @@
 const express = require('express');
 const Jimp = require('jimp');
-const { cv, cvTranslateError } = require('opencv-wasm');
+const { cv } = require('opencv-wasm');
 const fs = require('fs');
 const { performance } = require('perf_hooks');
 
 const app = express();
 const port = process.env.PORT || '8000';
 
-const run = async () => {
+const testFunc = async (funcName, func, json, args) => {
   let jimpSrc = await Jimp.read('../data/blackcat.jpg');
   let src = cv.matFromImageData(jimpSrc.bitmap);
   let dst = new cv.Mat();
 
-  const out = {};
   let power = 5;
   let numTimes = 10;
+
+  json[funcName] = {};
   for (let i = 0; i < power; i++) {
     let runs = Math.pow(numTimes, i);
     let start = performance.now();
     for (let j = 0; j < runs; j++) {
-      cv.transpose(src, dst);
+      func(src, dst, ...args);
     }
     let end = performance.now();
     let time = end - start;
-    out[`${runs}`] = { ms: time };
-    console.log(`opencv transpose time: ${time}`);
-    console.log(`finished transposing ${runs} times`);
+    json[funcName][runs] = { ms: time };
+    console.log(`opencv ${funcName} time: ${time}ms`);
+    console.log(`finished ${funcName} ${runs} times`);
   }
 
   src.delete();
   dst.delete();
+};
 
+const run = async () => {
+  const out = {};
+  await testFunc('transpose', cv.transpose, out, []);
+  await testFunc('grayscale', cv.cvtColor, out, [cv.COLOR_RGBA2GRAY, 0]);
   fs.writeFile('out.json', JSON.stringify(out), (err) => {
     if (err) {
       console.log(err);
