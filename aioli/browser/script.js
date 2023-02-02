9c8f2dc791a3b75a9a70a60472d1e117cbe64057
hamed <amzhamed@gmail.com>
2023-02-01 16:48:30 -0800

    add form to input data

--- aioli/browser/script.js
+++ aioli/browser/script.js
@@ -1,9 +1,5 @@
-const CLI = await new Aioli(["kalign/3.3.1"]);
-
-// Create sample data (source: https://github.com/TimoLassmann/kalign/blob/master/dev/data/BB11001.tfa)
-await CLI.mount({
-  name: "input.fa",
-  data: `>1aab_
+document.getElementById("name").value = "input.fa";
+document.getElementById("data").value = `>1aab_
 GKGDPKKPRGKMSSYAFFVQTSREEHKKKHPDASVNFSEFSKKCSERWKT
 MSAKEKGKFEDMAKADKARYEREMKTYIPPKGE
 >1j46_A
@@ -14,31 +10,43 @@ MKKLKKHPDFPKKPLTPYFRFFMEKRAKYAKLHPEMSNLDLTKILSKKYK
 ELPEKKKMKYIQDFQREKQEFERNLARFREDHPDLIQNAKK
 >2lef_A
 MHIKKPLNAFMLYMKEMRANVVAESTLKESAAINQILGRRWHALSREEQA
-KYYELARKERQLHMQLYPGWSARDNYGKKKKRKREK`
-});
+KYYELARKERQLHMQLYPGWSARDNYGKKKKRKREK`;
 
-// Run kalign and fetch FASTA file output
+const CLI = await new Aioli(["kalign/3.3.1"]);
 
-const rounds = 5;
-
-for (let i = 0; i < rounds; i++) {
-  const start = Date.now();
-
-  const tries = Math.pow(10, i);
-  for (let j = 0; j < tries; j++) {
-    await CLI.exec("kalign input.fa -f fasta -o result.fasta");
-    const result = await CLI.cat("result.fasta");
-    document.getElementById("output").innerHTML = result;
-  }
-
-  const end = Date.now();
-  const duration = end - start;
-
-  const message = `kalign ran ${tries} time(s) in ${duration} ms`;
-  log(message);
+async function mountData(data) {
+  await CLI.mount(data);
 }
 
-log('Done');
+async function run() {
+  const rounds = 5;
+
+  for (let i = 0; i < rounds; i++) {
+    const tries = Math.pow(10, i);
+
+    document.getElementById("output").innerHTML = "Loading...";
+    let message = `running ${tries} time(s) ...`;
+    log(message);
+
+    const start = Date.now();
+
+    let result;
+    for (let j = 0; j < tries; j++) {
+      await CLI.exec("kalign input.fa -f fasta -o result.fasta");
+      result = await CLI.cat("result.fasta");
+    }
+
+    const end = Date.now();
+    const duration = end - start;
+    document.getElementById("output").innerHTML = result;
+
+
+    message = `kalign ran ${tries} time(s) in ${duration} ms`;
+    log(message);
+  }
+
+  log('Done');
+}
 
 function log(text) {
   const pNode = document.createElement('p');
@@ -46,3 +54,18 @@ function log(text) {
   document.getElementById("logs").append(pNode);
   console.log(text)
 }
+
+async function submitForm(e) {
+  e.preventDefault();
+  const formData = {
+    name: document.getElementById("name").value,
+    data: document.getElementById("data").value,
+  };
+
+  document.getElementById("action").disabled = true;
+  await mountData(formData);
+  await run();
+  document.getElementById("action").disabled = false;
+}
+
+document.getElementById('dataForm').addEventListener("submit", submitForm);
