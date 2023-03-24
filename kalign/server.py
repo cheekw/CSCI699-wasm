01ceeeb9e01c629d06749050a85629605e98657b
Mojgan Asadi <masadi@usc.edu>
2023-03-24 15:29:21 -0700

    update kalign test

--- kalign/server.py
+++ kalign/server.py
@@ -3,6 +3,6 @@ import socketserver
 
 handler = http.server.SimpleHTTPRequestHandler
 handler.extensions_map['.wasm'] = 'application/wasm'
-httpd = socketserver.TCPServer(('', 8001), handler)
+httpd = socketserver.TCPServer(('', 8000), handler)
 httpd.serve_forever()
 
